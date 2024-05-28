6ca4077a765aca0eed7c67bb4d98f3a90def5eca
ar448467 <134503163+ar448467@users.noreply.github.com>
2024-05-28 09:18:20 +0200

    Update script3.ts

--- script3.ts
+++ script3.ts
@@ -1,32 +1,29 @@
-const imagesDiv = document.getElementById("images") as HTMLDivElement;
+"use strict";
+const imagesDiv = document.getElementById("images");
 const ws = new WebSocket("ws://127.0.0.1:7000/ws/images");
-const addImageButton = document.getElementById("addImage") as HTMLButtonElement;
-
-ws.onmessage = (event: MessageEvent) => {
+const addImageButton = document.getElementById("addImage");
+ws.onmessage = (event) => {
     console.log("WebSocket message received:", event.data);
     const imageContainer = document.createElement("div");
     imageContainer.classList.add("image-container");
     imageContainer.innerHTML = event.data;
     imagesDiv.insertBefore(imageContainer, imagesDiv.firstChild);
 };
-
 ws.onopen = () => {
     console.log("WebSocket connection opened");
 };
-
 ws.onclose = () => {
     console.log("WebSocket connection closed");
 };
-
 addImageButton.addEventListener("click", () => {
     console.log("Button clicked");
     fetch("http://127.0.0.1:7000/add-image")
         .then(response => {
-            console.log("Response status:", response.status);
-            return response.json();
-        })
+        console.log("Response status:", response.status);
+        return response.json();
+    })
         .then(data => {
-            console.log("Response from server:", data);
-        })
+        console.log("Response from server:", data);
+    })
         .catch(error => console.error("Error:", error));
 });
